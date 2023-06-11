3a00e272a992e7034fd2d8b9d097ec50d85aed30
Adhish Kapoor <adhish.kapoor@nagarro.com>
2023-06-11 19:55:37 +0530

    web-app YAML changes

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-#!/usr/bin/python
 import os
 from flask import Flask
 from peewee import MySQLDatabase, IntegerField
 
-MYSQL_ROOT_USER = os.getenv('MYSQL_ROOT_USER', 'root')
-MYSQL_ROOT_PASSWORD = os.getenv('MYSQL_ROOT_PASSWORD', 'admin')
-MYSQL_ROOT_HOST = os.getenv('MYSQL_ROOT_HOST', 'localhost')
-MYSQL_ROOT_PORT = os.getenv('MYSQL_ROOT_PORT', '3306')
-MYSQL_ROOT_DB = os.getenv('MYSQL_ROOT_DB', 'mydb')
-FLASK_APP_PORT = os.getenv('FLASK_APP_PORT', '8282')
+MYSQL_ROOT_USER = os.getenv('MYSQL_ROOT_USER')
+MYSQL_ROOT_PASSWORD = os.getenv('MYSQL_ROOT_PASSWORD')
+MYSQL_ROOT_HOST = os.getenv('MYSQL_ROOT_HOST')
+MYSQL_ROOT_PORT = os.getenv('MYSQL_ROOT_PORT')
+MYSQL_ROOT_DB = os.getenv('MYSQL_ROOT_DB')
+FLASK_APP_PORT = os.getenv('FLASK_APP_PORT')
 
 db = MySQLDatabase(database=MYSQL_ROOT_DB, user=MYSQL_ROOT_USER, password=MYSQL_ROOT_PASSWORD,
                    host=MYSQL_ROOT_HOST, port=int(MYSQL_ROOT_PORT))
@@ -16,10 +15,16 @@ db = MySQLDatabase(database=MYSQL_ROOT_DB, user=MYSQL_ROOT_USER, password=MYSQL_
 app = Flask(__name__)
 app.run(host='0.0.0.0', port=int(FLASK_APP_PORT))
 
-@app.route('/')
-def get_db_version():
+"""Function to test the functionality of the API"""
+@app.route("/")
+def index():
+    return "This is Home Page"
+
+"""Function to retrieve all users from the MySQL database"""
+@app.route('/users', methods=["GET"])
+def get_users():
     cursor = db.cursor()
-    cursor.execute("SELECT VERSION()")
-    data = cursor.fetchone()
+    cursor.execute("SELECT * FROM USERS")
+    data = cursor.fetchall()
     db.close()
-    return "Database version : %s " % data
+    return "Users are :\n %s " % data
