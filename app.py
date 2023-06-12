3b89ffb47ef3cac1bb2d3247db99db82dd88dd89
Adhish Kapoor <adhish.kapoor@nagarro.com>
2023-06-12 15:05:16 +0530

    code changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from peewee import MySQLDatabase
 
 MYSQL_ROOT_USER = os.getenv('MYSQL_ROOT_USER', 'root')
 MYSQL_ROOT_PASSWORD = os.getenv('MYSQL_ROOT_PASSWORD', 'admin')
-MYSQL_ROOT_HOST = os.getenv('MYSQL_ROOT_HOST', '10.61.128.194')
+MYSQL_ROOT_HOST = os.getenv('MYSQL_ROOT_HOST', 'localhost')
 MYSQL_ROOT_PORT = os.getenv('MYSQL_ROOT_PORT', '3306')
 MYSQL_ROOT_DB = os.getenv('MYSQL_ROOT_DB', 'mysql')
 FLASK_APP_PORT = os.getenv('FLASK_APP_PORT', '8282')
