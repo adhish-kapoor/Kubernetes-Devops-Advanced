73870fc34de436fae440bebdce0f891e3e4b1a6b
Adhish Kapoor <adhish.kapoor@nagarro.com>
2023-06-12 12:46:41 +0530

    code changes

--- app.py
+++ app.py
@@ -2,18 +2,17 @@ import os
 from flask import Flask, jsonify
 from peewee import MySQLDatabase, IntegerField
 
-MYSQL_ROOT_USER = os.getenv('MYSQL_ROOT_USER')
-MYSQL_ROOT_PASSWORD = os.getenv('MYSQL_ROOT_PASSWORD')
-MYSQL_ROOT_HOST = os.getenv('MYSQL_ROOT_HOST')
+MYSQL_ROOT_USER = os.getenv('MYSQL_ROOT_USER', 'root')
+MYSQL_ROOT_PASSWORD = os.getenv('MYSQL_ROOT_PASSWORD', 'admin')
+MYSQL_ROOT_HOST = os.getenv('MYSQL_ROOT_HOST', '10.61.128.194')
 MYSQL_ROOT_PORT = os.getenv('MYSQL_ROOT_PORT', '3306')
-MYSQL_ROOT_DB = os.getenv('MYSQL_ROOT_DB')
+MYSQL_ROOT_DB = os.getenv('MYSQL_ROOT_DB', 'mysql')
 FLASK_APP_PORT = os.getenv('FLASK_APP_PORT', '8282')
 
 db = MySQLDatabase(database=MYSQL_ROOT_DB, user=MYSQL_ROOT_USER, password=MYSQL_ROOT_PASSWORD,
                    host=MYSQL_ROOT_HOST, port=int(MYSQL_ROOT_PORT))
 
 app = Flask(__name__)
-app.run(host='0.0.0.0', port=int(FLASK_APP_PORT))
 
 """Function to test the functionality of the API"""
 @app.route("/")
@@ -30,3 +29,6 @@ def get_users():
     response = jsonify(data)
     response.status_code = 200
     return "Users are :\n %s " % response
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(FLASK_APP_PORT))
